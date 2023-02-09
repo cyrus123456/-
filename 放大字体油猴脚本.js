a8ff19bfbc8ce74f6b69e3d66b0241b1529c2567
王珠峰 <734765725@qq.com>
2023-02-09 18:08:23 +0800

    0.2.4

--- 放大字体油猴脚本.js
+++ 放大字体油猴脚本.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         新东方词库单词字体放大
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1.1
+// @version      0.2.4
 // @description  https://www.koolearn.com/单词字体放大
 // @author       You
 // @match        https://www.koolearn.com/dict/*
@@ -13,24 +13,31 @@
 (function () {
   'use strict';
   // Your code here...
+
+  //单词列表放大
   for (const element of document.querySelector(".word-box").querySelectorAll('.word')) {
     element.style.fontSize = "40px"
     element.style.padding = "10px"
+    element.style.margin = "30px 0"
   }
 
+  //单词详情
+  //单词
   document.querySelector(".content-box iframe").style.display = 'none'
   document.querySelector(".content-wrap").querySelector(".left-content").querySelector(".word-title").querySelector(".word-spell").style.fontSize = "100px"
-  //document.querySelector(".content-wrap").querySelector(".left-content").document.querySelector(".content-box").querySelector(".details-content").querySelector(".word-spell-box").querySelector(".word-spell-audio").style.width = "100px"
-  //document.querySelector(".content-wrap").querySelector(".left-content").document.querySelector(".content-box").querySelector(".details-content").querySelector(".details-content-title-box").style.width = "100px"
-  document.querySelector(".word-spell-audio").style.heigh = "100px"
-
+  //翻译
   document.querySelector(".details-content-title-box p").style.fontSize = "50px"
+  document.querySelector(".details-content-title-box p").style.lineHeight = "70px"
+  //例句
+  document.querySelectorAll(".details-content-title-box div ol")[1].style.fontSize = "50px"
+  document.querySelectorAll(".details-content-title-box div ol")[1].style.lineHeight = "70px"
+  for (const element of document.querySelectorAll(".details-content-title-box div ol")[1].querySelectorAll("li")) {
+    for (const elementelement of element.querySelectorAll("li")) {
 
-  console.log(123313, document.querySelector(".details-content-title-box"))
+      elementelement.style.margin = "40px 0"
+    }
+  }
+  //打印
+  //console.log(document.querySelectorAll(".details-content-title-box div ol")[1].querySelectorAll("li"))
 
-
-
-
-  //document.querySelector(".content-wrap").querySelector(".left-content").style.width = '400px';
-  // document.querySelector(".left-content").style.width = '400px';
 })();
